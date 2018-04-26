05bab0c8eba4f4ba92517b4fd69ecdb6f2844538
zylo117 <zylo117@hotmail.com>
2018-04-26 17:05:35 +0800

    update

--- brain/anticipator/test2.py
+++ brain/anticipator/test2.py
@@ -2,6 +2,20 @@ import job.merge_data as merge_data
 import pandas as pd
 import numpy as np
 
+gfc_lcb_items = ["GFC-UP Input Quantity",
+                 "10_Bin17_LCB(center)",
+                 "11_Bin18_LCB(edge)",
+                 "12_Bin19_LCB(corner)",
+                 "76_Bin22_LCBraw",
+                 "77_Bin23_LCBaccum",
+                 "GFC-DOWN Input Quantity",
+                 "01_Bin17_LCB(center)",
+                 "02_Bin18_LCB(edge)",
+                 "03_Bin19_LCB(corner)",
+                 "77_Bin22_LCBraw",
+                 "78_Bin23_LCBaccum",
+                 "79_Bin25_LCB_Retest_Count"]
+
 if __name__ == "__main__":
     data_path = "c:\\temp\\"
 
@@ -18,10 +32,11 @@ if __name__ == "__main__":
     fusion_data = merge_data.merge_gfc_data(material_data, gfc_up_csv_path, gfc_down_csv_path)
 
     # pre-process starts
-    fusion_data["LCB_UP_QTY"] = fusion_data["10_Bin17_LCB(center)"] + fusion_data["11_Bin18_LCB(edge)"] + fusion_data["12_Bin19_LCB(corner)"] + fusion_data["76_Bin22_LCBraw"] + fusion_data["77_Bin23_LCBaccum"]
-    fusion_data["LCB_DOWN_QTY"] = fusion_data["01_Bin17_LCB(center)"]+fusion_data["02_Bin18_LCB(edge)"]+fusion_data["03_Bin19_LCB(corner)"]+fusion_data["77_Bin22_LCBraw"]+fusion_data["78_Bin23_LCBaccum"]+fusion_data["79_Bin25_LCB_Retest_Count"]
-
+    fusion_data["LCB_UP_QTY"] = fusion_data["10_Bin17_LCB(center)"] + fusion_data["11_Bin18_LCB(edge)"] + fusion_data[
+        "12_Bin19_LCB(corner)"] + fusion_data["76_Bin22_LCBraw"] + fusion_data["77_Bin23_LCBaccum"]
+    fusion_data["LCB_DOWN_QTY"] = fusion_data["01_Bin17_LCB(center)"] + fusion_data["02_Bin18_LCB(edge)"] + fusion_data[
+        "03_Bin19_LCB(corner)"] + fusion_data["77_Bin22_LCBraw"] + fusion_data["78_Bin23_LCBaccum"] + fusion_data[
+                                      "79_Bin25_LCB_Retest_Count"]
 
     fuck = pd.pivot_table(fusion_data, index=["Category_Config"], columns=["GFC-UP Process Date 7:00"], aggfunc=np.sum)
     print(0)
-
