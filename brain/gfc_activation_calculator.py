51f27e3c735ed29c318fe20c901a672fd800b24f
zylo117 <zylo117@hotmail.com>
2018-04-29 18:15:39 +0800

    update logic

--- brain/gfc_activation_calculator.py
+++ brain/gfc_activation_calculator.py
@@ -7,9 +7,11 @@ import numpy as np
 
 # assume every product was tested for 5 secs
 
-def cal_act(gfc_data_path, from_time=None, to_time=None):
-    raw_data = pd.read_csv(gfc_data_path)
-    data_by_mac = pd.pivot_table(raw_data, index=raw_data["設備"], values=["2D CODE"], aggfunc=len)
+def cal_act(gfc_data, from_time=None, to_time=None, category=None):
+    if category is not None:
+        gfc_data = gfc_data[gfc_data["機種"].apply(lambda x: str(x).lower()) == category.lower()]
+
+    data_by_mac = pd.pivot_table(gfc_data, index=gfc_data["設備"], values=["2D CODE"], aggfunc=len)
     mac_name_list = data_by_mac._stat_axis._data
     result = {}
 
@@ -21,7 +23,9 @@ def cal_act(gfc_data_path, from_time=None, to_time=None):
         to_time = datetime.datetime.strptime(str(datetime.date.today() + datetime.timedelta(days=1)) + " 00:00:00", "%Y-%m-%d %H:%M:%S")
 
     for mac_name in mac_name_list:
-        tmp_data = raw_data[raw_data["設備"] == mac_name]
+        pd.options.mode.chained_assignment = None
+
+        tmp_data = gfc_data[gfc_data["設備"] == mac_name]
         tmp_data["日期"] = tmp_data["日期"].apply(lambda x: datetime.datetime.strptime(x, "%Y/%m/%d %H:%M:%S"))
 
         tmp_data = tmp_data.loc[tmp_data["日期"] > from_time].loc[tmp_data["日期"] < to_time]
@@ -29,14 +33,20 @@ def cal_act(gfc_data_path, from_time=None, to_time=None):
         ttl_qty = np.count_nonzero(tmp_data["BinNo"] > 0)
         ok_qty = np.count_nonzero(tmp_data["BinNo"] == 1)
 
-        activation = ok_qty / ttl_qty
+        if ttl_qty == 0:
+            result[mac_name] = 0
 
-        result[mac_name] = activation
+        else:
+            activation = ok_qty / ttl_qty
+
+            result[mac_name] = activation
 
     return result
 
 
 if __name__ == "__main__":
-    result = cal_act("F:/Document/GitHub/ZombieZoo/face/000.csv", from_time="2018-04-28 12:50:50", to_time="2018-04-28 18:50:50")
+    gfc_data = pd.read_csv("F:/Document/GitHub/ZombieZoo/face/000.csv")
+    result = cal_act(gfc_data, from_time="2018-04-29 12:50:50",
+                     to_time="2018-04-30 18:50:50", category="GRanite-E")
     # cal_act("F:/Document/GitHub/ZombieZoo/face/000.csv")
     print(0)
--- face/activation_monitor_logic.py
+++ face/activation_monitor_logic.py
@@ -1,6 +1,8 @@
 # load UI
+import datetime
 import sys
 import numpy as np
+import pandas as pd
 
 from PyQt5 import QtCore
 from PyQt5.QtCore import Qt
@@ -9,12 +11,20 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
 from face.activation_monitor import Ui_MainWindow
 from brain.gfc_activation_calculator import cal_act
 
-category_header = {"Granite": "Q", "BlueBerry": "Q", "Lumber": "H", "Angel": "A", "Syrup": "Y"}
-mac_type = {"GFC-UP": "GU", "GFC-DOWN": "GD"}
+category_header = {"Granite": "Q",
+                   "BlueBerry": "Q",
+                   "Lumber": "H",
+                   "Angel": "A",
+                   "Syrup": "Y"}
+mac_type = {"GFC-UP": "GU", "GFC-DOWN": "GD",
+            "OQC-UP": "QU", "OQC-DOWN": "QD",
+            "CUBE-UP": "CU", "CUBE-DOWN": "CD",
+            "REL-UP": "RU", "REL-DOWN": "RD"}
 mac_no_total = 60
 default_gfc_data_csv_path = "F:/Document/GitHub/ZombieZoo/face/000.csv"
 from_time = "2018/04/28 12:50:50"
 to_time = "2018/04/28 18:50:50"
+
 
 class MainWindow(QMainWindow, Ui_MainWindow):
 
@@ -22,33 +32,43 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         super(MainWindow, self).__init__(parent)
         self.setupUi(self)
 
-        self.category = self.category_select.currentText().split("-")[0]
+        self.config = self.category_select.currentText()
+        self.category = self.config.split("-")[0]
         self.mac_type = self.mac_type_select.currentText()
         self.mac_name_list = []
         self.activation_status_list = []
         self.activation_val_list = []
 
+        self.data_loaded = False
+        self.gfc_data = None
+
+        # init
+        self.update_date()
         self.add_all_activation()
 
         self.category_select.currentIndexChanged.connect(lambda x: self.change_all_activation())
+        self.mac_type_select.currentIndexChanged.connect(lambda x: self.change_all_activation())
 
+        # buttons events
         self.auto_load_data.clicked.connect(lambda x: self.show_act_val())
         self.manual_load_data.clicked.connect(lambda x: self.show_act_val(manual=True))
+        self.reload.clicked.connect(self.reload_data)
 
     def add_all_activation(self):
         for i in range(mac_no_total):
-            mac_name = QLabel("%s%s" % (category_header[self.category], mac_type[self.mac_type] + str(i + 1).zfill(2) + "XX"))
+            mac_name = QLabel()
+            mac_name.setText("%s%s" % (category_header[self.category], mac_type[self.mac_type] + str(i + 1).zfill(2) + "XX"))
             mac_name.setMinimumSize(QtCore.QSize(0, 50))
             self.gridLayout.addWidget(mac_name, i, 0, 1, 1)
             self.gridLayout.setColumnStretch(0, 1)
             self.mac_name_list.append(mac_name)
 
-            activation_status = QLabel(self.scrollAreaWidgetContents)
+            activation_status = QLabel()
             self.gridLayout.addWidget(activation_status, i, 1, 1, 1)
             self.activation_status_list.append(activation_status)
 
-            activation_val = QLabel(self.scrollAreaWidgetContents)
-            activation_val.setAlignment(QtCore.Qt.AlignVCenter|QtCore.Qt.AlignRight)
+            activation_val = QLabel()
+            activation_val.setAlignment(QtCore.Qt.AlignVCenter | QtCore.Qt.AlignRight)
             activation_val.setText("0 % ")
             self.gridLayout.addWidget(activation_val, i, 2, 1, 1)
             self.activation_val_list.append(activation_val)
@@ -57,22 +77,30 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.gridLayout.setColumnStretch(1, 9)
         self.gridLayout.setColumnStretch(2, 1)
 
-
     def change_all_activation(self):
-        self.category = self.category_select.currentText().split("-")[0]
+        self.config = self.category_select.currentText()
+        self.category = self.config.split("-")[0]
         self.mac_type = self.mac_type_select.currentText()
         for i in range(mac_no_total):
             self.mac_name_list[i].setText(
                 "%s%s" % (category_header[self.category], mac_type[self.mac_type] + str(i + 1).zfill(2) + "XX"))
+            self.mac_name_list[i].setHidden(False)
+            self.activation_status_list[i].setHidden(False)
+            self.activation_val_list[i].setHidden(False)
 
     def show_act_val(self, manual=False):
+        if not self.data_loaded:
+            self.gfc_data = pd.read_csv(default_gfc_data_csv_path)
+            self.data_loaded = True
+
         if manual:
             from_time = self.from_time.dateTime().toString(Qt.ISODate).replace("T", " ")
             to_time = self.to_time.dateTime().toString(Qt.ISODate).replace("T", " ")
 
-            act_result = cal_act(default_gfc_data_csv_path, from_time, to_time)
+            act_result = cal_act(self.gfc_data, from_time, to_time, category=self.config)
         else:
-            act_result = cal_act(default_gfc_data_csv_path)
+            self.gfc_data = pd.read_csv(default_gfc_data_csv_path)
+            act_result = cal_act(self.gfc_data, category=self.config)
 
         for i in range(mac_no_total):
             try:
@@ -80,7 +108,34 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                 activation = str(np.round(activation * 100, 2)) + " % "
                 self.activation_val_list[i].setText(activation)
             except:
+                self.mac_name_list[i].setHidden(True)
+                self.activation_status_list[i].setHidden(True)
+                self.activation_val_list[i].setHidden(True)
                 continue
+
+    def reload_data(self):
+        self.gfc_data = pd.read_csv(default_gfc_data_csv_path)
+        self.data_loaded = True
+
+    def update_date(self):
+        # # get current date and time
+        # now = QtCore.QDateTime.currentDateTime()
+        # # set date only
+        # today = QtCore.QDate.currentDate()
+        # # set time only
+        # this_moment = QtCore.QTime.currentTime()
+        # # set an arbitrary date
+        # some_date = QtCore.QDate(2011, 4, 22)  # Year, Month, Day
+        # # set an arbitrary time
+        # some_time = QtCore.QTime(16, 33, 15)  # Hours, Minutes, Seconds (Only H and M required)
+        # # set an arbitrary date and time
+        # someDT = QtCore.QDateTime(2011, 4, 22, 16, 33, 15)
+
+        from_time = datetime.datetime.strptime(str(datetime.date.today()) + " 00:00:00", "%Y-%m-%d %H:%M:%S")
+        to_time = datetime.datetime.strptime(str(datetime.date.today() + datetime.timedelta(days=1)) + " 00:00:00", "%Y-%m-%d %H:%M:%S")
+
+        self.from_time.setDateTime(from_time)
+        self.to_time.setDateTime(to_time)
 
 app = QApplication(sys.argv)
 mainWindow = MainWindow()
